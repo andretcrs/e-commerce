7348730dbc1b97b4cc7cbd5a9e382d16fc05a953
André Scheffer <andretcrs@gmail.com>
2022-01-11 14:16:03 -0300

    ajustado a suite home para os padroes do cypress.json

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,8 +1,7 @@
 describe("home page", () => {
   it("app deve estar online", () => {
     //it descreve o caso de teste
-    cy.viewport(1440, 900); // dimensiona o tamanho da tela
-    cy.visit("https://buger-eats.vercel.app");
-    cy.get("#page-home main h1").should("have.text", "Seja um parceiro entregador pela Buger Eats"); // .should é um assert
+    cy.visit('/');
+    cy.get('h1').should("have.text", "Seja um parceiro entregador pela Buger Eats"); // .should é um assert
   });
 });
